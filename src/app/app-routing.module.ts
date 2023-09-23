1bc9af43be3e4229b103dbb1c664a926917f0e9a
Hatim <hatimjuzer2@gmail.com>
2023-09-23 16:34:30 +0530

    temp change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"menu",loadChildren:()=>import("./modules/menu/menu.module").then((m)=>m.MenuModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
