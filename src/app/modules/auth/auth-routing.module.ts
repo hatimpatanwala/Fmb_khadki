141b52e2ab5a3f0c59b0b35d74496c51c88874a0
unknown <hatim@beatroute.io>
2023-09-26 09:14:28 +0530

    component added

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { VerifyOtpComponent } from './components/verify-otp/verify-otp.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/login',
+    component: LoginComponent,
+  },
+  {
+    path: '/verify-otp',
+    component: VerifyOtpComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
